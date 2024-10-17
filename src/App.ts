622e9a7ad056f423735f477a4656ad687d54a4a9
ethan <18765895345lb@gmail.com>
2024-10-17 16:37:36 -0400

    Finished tokenizer

--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,27 @@
-import chalk from "chalk";
-import KevlarLexer from "./lexer/Tokenizer.js";
+import fs from 'fs';
+import chalk from 'chalk';
+import cliSpinners from 'cli-spinners';
+import terminal from './utils/terminal.js';
+import compile from './compile.js';
 
-console.log(JSON.stringify( KevlarLexer.tokenize(`
-    uint<32> integer = 0;
-    int integer = 0;
-`), null, 4));
+
+if (process.argv.length <= 2) {
+    terminal.err('Bad usage: no file path specified')
+    process.exit(1);
+}
+
+const path = process.argv.pop();
+
+try {
+    if (!fs.statSync(path!).isFile()){
+        terminal.err(`Bad usage: "${path}" is not a file`)
+        process.exit(1);
+    }
+
+    compile(fs.readFileSync(path!, 'utf8'));
+
+} catch (err) {
+    // @ts-ignore
+    terminal.err(err);
+}
 
